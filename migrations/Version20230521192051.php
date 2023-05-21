5f0de889d47f5ae34cac084105dd95b52200a26a
ahmed <abdeliahmed204@gmail.com>
2023-05-21 20:42:12 +0100

    message

--- migrations/Version20230521192051.php
+++ migrations/Version20230521192051.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230520220235 extends AbstractMigration
+final class Version20230521192051 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,38 +20,26 @@ final class Version20230520220235 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE booking DROP FOREIGN KEY FK_E00CEDDE9D86650F');
-        $this->addSql('ALTER TABLE booking DROP FOREIGN KEY FK_E00CEDDE75D4A2B8');
         $this->addSql('CREATE TABLE customer (id INT AUTO_INCREMENT NOT NULL, package_chosen_id INT DEFAULT NULL, nom VARCHAR(255) NOT NULL, prenom VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, INDEX IDX_81398E09C964F846 (package_chosen_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE travel_package (id INT AUTO_INCREMENT NOT NULL, destination VARCHAR(255) NOT NULL, start_date DATE NOT NULL, end_date DATE NOT NULL, price DOUBLE PRECISION NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE travel_package (id INT AUTO_INCREMENT NOT NULL, destination VARCHAR(255) NOT NULL, start_date DATE NOT NULL, end_date DATE NOT NULL, price DOUBLE PRECISION NOT NULL, img_src VARCHAR(255) DEFAULT NULL, description VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE customer ADD CONSTRAINT FK_81398E09C964F846 FOREIGN KEY (package_chosen_id) REFERENCES travel_package (id)');
+        $this->addSql('ALTER TABLE booking DROP FOREIGN KEY FK_E00CEDDE75D4A2B8');
+        $this->addSql('ALTER TABLE booking DROP FOREIGN KEY FK_E00CEDDE9D86650F');
+        $this->addSql('DROP TABLE booking');
         $this->addSql('DROP TABLE user');
         $this->addSql('DROP TABLE voyage');
-        $this->addSql('DROP INDEX IDX_E00CEDDE9D86650F ON booking');
-        $this->addSql('DROP INDEX IDX_E00CEDDE75D4A2B8 ON booking');
-        $this->addSql('ALTER TABLE booking ADD customer_id INT DEFAULT NULL, ADD travel_package_id INT DEFAULT NULL, DROP user_id_id, DROP voyage_id_id, DROP booking_date');
-        $this->addSql('ALTER TABLE booking ADD CONSTRAINT FK_E00CEDDE9395C3F3 FOREIGN KEY (customer_id) REFERENCES customer (id)');
-        $this->addSql('ALTER TABLE booking ADD CONSTRAINT FK_E00CEDDEADF94A2C FOREIGN KEY (travel_package_id) REFERENCES travel_package (id)');
-        $this->addSql('CREATE INDEX IDX_E00CEDDE9395C3F3 ON booking (customer_id)');
-        $this->addSql('CREATE INDEX IDX_E00CEDDEADF94A2C ON booking (travel_package_id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE booking DROP FOREIGN KEY FK_E00CEDDE9395C3F3');
-        $this->addSql('ALTER TABLE booking DROP FOREIGN KEY FK_E00CEDDEADF94A2C');
+        $this->addSql('CREATE TABLE booking (id INT AUTO_INCREMENT NOT NULL, user_id_id INT NOT NULL, voyage_id_id INT NOT NULL, booking_date DATE NOT NULL, INDEX IDX_E00CEDDE9D86650F (user_id_id), INDEX IDX_E00CEDDE75D4A2B8 (voyage_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, prenom VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, email VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, pwd VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, tel INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
         $this->addSql('CREATE TABLE voyage (id INT AUTO_INCREMENT NOT NULL, destination VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, start_date DATE NOT NULL, end_date DATE NOT NULL, price DOUBLE PRECISION NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('ALTER TABLE booking ADD CONSTRAINT FK_E00CEDDE75D4A2B8 FOREIGN KEY (voyage_id_id) REFERENCES voyage (id)');
+        $this->addSql('ALTER TABLE booking ADD CONSTRAINT FK_E00CEDDE9D86650F FOREIGN KEY (user_id_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE customer DROP FOREIGN KEY FK_81398E09C964F846');
         $this->addSql('DROP TABLE customer');
         $this->addSql('DROP TABLE travel_package');
-        $this->addSql('DROP INDEX IDX_E00CEDDE9395C3F3 ON booking');
-        $this->addSql('DROP INDEX IDX_E00CEDDEADF94A2C ON booking');
-        $this->addSql('ALTER TABLE booking ADD user_id_id INT NOT NULL, ADD voyage_id_id INT NOT NULL, ADD booking_date DATE NOT NULL, DROP customer_id, DROP travel_package_id');
-        $this->addSql('ALTER TABLE booking ADD CONSTRAINT FK_E00CEDDE9D86650F FOREIGN KEY (user_id_id) REFERENCES user (id)');
-        $this->addSql('ALTER TABLE booking ADD CONSTRAINT FK_E00CEDDE75D4A2B8 FOREIGN KEY (voyage_id_id) REFERENCES voyage (id)');
-        $this->addSql('CREATE INDEX IDX_E00CEDDE9D86650F ON booking (user_id_id)');
-        $this->addSql('CREATE INDEX IDX_E00CEDDE75D4A2B8 ON booking (voyage_id_id)');
     }
 }
