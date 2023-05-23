10885cf38a5c6e82268f3320da02dde70fdf1358
ahmed <abdeliahmed204@gmail.com>
2023-05-23 21:58:58 +0100

    Commit

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -1,13 +1,13 @@
 <?php
 
 namespace App\Controller;
-
 use App\Entity\User;
 use App\Form\RegistrationType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 
 class SecurityController extends AbstractController
@@ -15,18 +15,23 @@ class SecurityController extends AbstractController
    /**
      * @Route("/inscription", name="security_registration")
      */
-    public function registration(Request $request, EntityManagerInterface  $em)
-    {
-        $user = new User();
-        $form  = $this->createForm(RegistrationType::class, $user);
-        $form->handleRequest($request);
-  
-        if($form->isSubmitted() && $form->isValid()) {
-        //l'objet $em sera affecté automatiquement grâce à l'injection des dépendances de symfony 4  
-           $em->persist($user);
-           $em->flush();  
-        }
-       return $this->render('security/registration.html.twig', 
-                           ['form' =>$form->createView()]);
+    public function registration(Request $request, EntityManagerInterface  $em, 
+    UserPasswordEncoderInterface $encoder)
+{
+    $user = new User();
+    $form  = $this->createForm(RegistrationType::class, $user);
+    $form->handleRequest($request);
+
+    if($form->isSubmitted() && $form->isValid()) {
+        $hash = $encoder->encodePassword($user,$user->getPassword());
+        $user->setPassword($hash);
+    //l'objet $em sera affecté automatiquement grâce à l'injection des dépendances de symfony 4  
+       $em->persist($user);
+       $em->flush();  
+
     }
+   return $this->render('security/registration.html.twig', 
+                       ['form' =>$form->createView()]);
+}
+
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,17 +2,23 @@
 
 namespace App\Entity;
 
-use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
- * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @UniqueEntity(
+ *     fields={"email"},
+ *     message="L'émail que vous avez tapé est déjà utilisé !"
+ * )
  */
-class User
+class User implements UserInterface
 {
     /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
      */
     private $id;
@@ -26,11 +32,21 @@ class User
      * @ORM\Column(type="string", length=255)
      */
     private $username;
+    
 
     /**
      * @ORM\Column(type="string", length=255)
+     *  @Assert\Length(
+     *      min = 8,
+     *      minMessage = "Votre mot de passe doit comporter au minimum {{ limit }} caractères")
+     *  @Assert\EqualTo(propertyPath = "confirm_password", message="Vous n'avez pas passé le même mot de passe !" )
      */
     private $password;
+
+
+     /**
+      *  @Assert\EqualTo(propertyPath = "password", message="Vous n'avez pas passé le même mot de passe !" )
+     */
     private $confirm_password;
 
     public function getConfirmPassword()
@@ -44,6 +60,7 @@ class User
 
         return $this;
     }
+
 
 
     public function getId(): ?int
@@ -86,4 +103,15 @@ class User
 
         return $this;
     }
+
+    public function getRoles()
+    {
+        return ['ROLE_USER'];
+    }
+
+    public function eraseCredentials() {}
+
+    public function getSalt() {}
+
+   
 }
