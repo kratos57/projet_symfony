539eb8b0b4e065680f7871e516ac6e443e82f6f0
ahmed <abdeliahmed204@gmail.com>
2023-05-25 13:07:34 +0100

    Commit

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -39,30 +39,6 @@ class HomeController extends AbstractController
             'destination' => $destination,
         ]);
     }
-    /**
-     * Route('/voyage/reservation', name: 'voyage_reservation')
-     */
-    public function reservation(Request $request): Response
-    {
-
-        $destination = $request->request->get('destination');
-        $prix = $request->request->get('prix');
-
-        // Récupérer les informations sur le client qui effectue la réservation
-        $user = $this->getUser(); // Supposons que vous utilisez l'authentification
-
-        // Créer et enregistrer la réservation
-        $reservation = new Reservation();
-    
-        $reservation->setDestination($destination);
-        $reservation->setPrix($prix);
-
-        $entityManager = $this->getDoctrine()->getManager();
-        $entityManager->persist($reservation);
-        $entityManager->flush();
-
-        // Réponse de confirmation
-        return new Response('Réservation effectuée avec succès !');
-    }
+  
 
 }
